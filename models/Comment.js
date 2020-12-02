67c96da8d1afd9338842dd4968031bc4d7e8acaf
Duke Nukem <garrettduke87@gmail.com>
2020-12-01 17:22:54 -0700

    set up the comment model

---
+++ models/Comment.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+// these are the objects created with each comment
+const CommentSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    commentBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// create the Comment model using CommentSchema
+const Comment = model('Comment', CommentSchema);
+
+// export the Comment model
+module.exports = Comment;
